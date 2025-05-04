bf64d9ea69994fdbc015976d9a1e48afcf5b86ea
youserstack <youserstack@gmail.com>
2025-05-04 22:41:53 +0900

    update

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM openjdk:17-jdk-slim
+
+# 워킹디렉토리 설정
+WORKDIR /app
+
+# 빌드한 .jar 파일을 컨테이너로 복사
+COPY build/libs/toopa-0.0.1-SNAPSHOT.jar toopa.jar
+
+# 환경변수
+# ENV SPRING_PROFILES_ACTIVE=prod
+
+# 애플리케이션 실행
+# 배포환경, 운영환경 : JVM 시스템 프로퍼티로 환경변수를 지정 (-D : Define)
+ENTRYPOINT ["java", "-Dspring.profiles.active=prod", "-jar", "/app/toopa.jar"]
+# 개발환경 : 커맨드라인 인자로 환경변수를 지정 : --spring.profiles.active=prod (최고우선순위)
+# ENTRYPOINT ["java", "-jar", "/app/toopa.jar","--spring.profiles.active=prod"]
+
+# 포트 8080 개방
+EXPOSE 8080
--- src/main/java/com/youserstack/toopa/config/WebConfig.java
+++ src/main/java/com/youserstack/toopa/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.youserstack.toopa.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,14 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+  @Value("${spring.cors.allowed-origins}")
+  private String[] allowedOrigins;
+
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**") // 모든 경로에 대해
-        .allowedOrigins("http://localhost:3000") // 프론트 주소
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins) // 쉼표로 여러 origin 가능
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        // .allowedHeaders("*")
-        .allowedHeaders("Authorization", "Content-Type") // <- 이 부분 중요
-        .allowCredentials(true); // 쿠키, 인증 포함 시 true
+        .allowedHeaders("Authorization", "Content-Type")
+        .allowCredentials(true);// 쿠키, 인증 포함 시 true
   }
-
 }
